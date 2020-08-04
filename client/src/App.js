d0ae9061efd49726d168adfc36f4cd7853f415e9
Milton Chang <mchangpi@gmail.com>
2020-08-04 16:20:16 +0800

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,158 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 500,
+      },
+    },
+  },
+};
+
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: [],
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: 0,
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  },
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = (u) => {
+    this.setState({
+      user: {
+        id: u.id,
+        name: u.name,
+        email: u.email,
+        entries: u.entries,
+        joined: u.joined,
+      },
+    });
+  };
+
+  calcFaceLocation = (data) => {
+    const face = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height, face);
+    return {
+      leftCol: face.left_col * 100 + "%",
+      rightCol: (1 - face.right_col) * 100 + "%",
+      topRow: face.top_row * 100 + "%",
+      bottomRow: (1 - face.bottom_row) * 100 + "%",
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch("http://localhost:3000/imageurl", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp) {
+          fetch("http://localhost:3000/image", {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((resp) => resp.json())
+            .then((count) => {
+              this.setState({
+                user: { ...this.state.user, entries: count },
+              });
+            })
+            .catch(console.log);
+        }
+        this.displayFaceBox(this.calcFaceLocation(resp));
+      })
+      .catch((error) => console.log(error));
+  };
+
+  onRouteChange = (route) => {
+    if (route === "home") {
+      this.setState({ isSignedIn: true });
+    } else {
+      this.setState(initialState);
+    }
+
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <Particles params={particlesOptions} className="particles" />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} box={box} />:
+          </div>
+        ) : route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
-require("dotenv").config();
+//require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,27 +18,34 @@ app.use(cors());
 let db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "milton",
-    password: "milton",
-    database: "smart-brain",
+    host: "satao.db.elephantsql.com",
+    port: 5432,
+    user: "tpqomhwb",
+    password: "Hude8-38yP7xVycOipEyXMO7isUJ67Aw",
+    database: "tpqomhwb",
   },
 });
 
-app.listen(process.env["SERVER_PORT"], () => {
-  console.log("node is listening on port " + process.env["SERVER_PORT"]);
+app.listen(PORT, () => {
+  console.log("node is listening on port " + PORT);
 });
 
-//signin -> POST ok/fail
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("/", (req, resp) =>
+  resp.send("<h2>Node is listening on port: " + PORT + "</h2>")
+);
+
 app.post("/signin", signin.handleSignin(db, bcrypt));
 
-//register -> POST user
 app.post("/register", register.handleRegister(db, bcrypt));
 
-//profile/:id -> GET user
 app.get("/profile/:id", profile.handleGet(db));
 
 //image -> POST clarifai api
 app.post("/imageurl", image.handleApi);
-//image -> PUT user
+
+//image -> PUT user entries
 app.put("/image", image.handlePut(db));
